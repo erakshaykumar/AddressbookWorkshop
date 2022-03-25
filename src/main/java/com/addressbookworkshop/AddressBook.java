30a3c37ddf8df04680bca3f3c63dcdb5ebda8b64
erakshaykumar <akaakshay304@gmail.com>
2022-03-25 09:11:15 +0530

    Added : Welcome Message

---
+++ src/main/java/com/addressbookworkshop/AddressBook.java
@@ -0,0 +1,13 @@
+/* @Purpose : Welcome Message to AddressBook
+ * @File : Address Book Program
+ * @Author : Akshay Kumar
+ */
+package com.addressbookworkshop;
+
+public class AddressBook {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To AddressBook ");
+	}
+
+}
---
+++ src/test/java/com/addressbookworkshop/AddressBookTest.java
@@ -0,0 +1,9 @@
+/* @Purpose : Welcome Message to AddressBook
+ * @File : Address Book Program
+ * @Author : Akshay Kumar
+ */
+package com.addressbookworkshop;
+
+public class AddressBookTest {
+
+}
