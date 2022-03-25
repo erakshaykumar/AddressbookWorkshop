8151c2a782382933cf61070ccbe0c3ddb6acb139
erakshaykumar <akaakshay304@gmail.com>
2022-03-25 09:41:12 +0530

    Added: Exception Handiling In Test Case

--- src/test/java/com/addressbookworkshop/AddressBookTest.java
+++ src/test/java/com/addressbookworkshop/AddressBookTest.java
@@ -12,13 +12,17 @@ import org.junit.Test;
 public class AddressBookTest {
 	@Test
 	public void givenContactCheckifItIsCorrect() {
-		Contact contacts = new Contact("akshay", "Kumar", "Gandak Project", "Muzaffarpur", "Bihar", 842002,
-				"7057082582", "abc@gmail.com");
-		AddressBook addressBook = new AddressBook();
-		addressBook.addContact(contacts);
-		List<Contact> contactList = addressBook.getcontactList();
-		Assert.assertTrue(contactList.contains(contacts));
-		System.out.println(contactList);
-	}
+		try {
+			Contact contacts = new Contact("akshay", "Kumar", "Gandak Project", "Muzaffarpur", "Bihar", 842002,
+					"7057082582", "abc@gmail.com");
+			AddressBook addressBook = new AddressBook();
+			addressBook.addContact(contacts);
+			List<Contact> contactList = addressBook.getcontactList();
+			Assert.assertTrue(contactList.contains(contacts));
+			System.out.println(contactList);
+		} catch (Exception e) {
+			System.out.println("Issue In Adding Contacts");
+		}
 
+	}
 }
